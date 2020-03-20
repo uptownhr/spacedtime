f89f44f8074ae3f69419cd0773a1361958488a90
James <james@Jamess-MacBook-Pro.local>
2020-03-19 22:03:39 -0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+      <h1>Spaced Time</h1>
+      <p>Remember the important things in your life. Using time spaced repition techniques, you can permemantly store information in your brain, instead of the cloud.</p>
+  </div>
+</template>
