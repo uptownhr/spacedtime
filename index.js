2bb9dad5cfe9044e48707c9e06f83d9a498d20b1
James Lee <uptownhr@gmail.com>
2020-03-21 14:24:52 -0700

    added ability to write to homedir/.spacedtime file

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 #!/usr/bin/env node
-
+const fs = require('fs')
+const os = require('os')
 const { program } = require('commander');
-program.version('0.0.1');
 
-program
-    .command('add <question> [answer]')
-    .description('clone a repository into a newly created directory')
-    .action((question, answer) => {
-        console.log('clone command called', question, answer);
-    });
+const homeDir = os.homedir()
+const timeSpacedConfigFilePath = homeDir + '/.timespaced'
 
-program.parse(process.argv);
+let data
+
+function saveData (data) {
+    const json = JSON.stringify(data)
+    fs.writeFileSync(timeSpacedConfigFilePath, json)
+}
+
+function loadData () {
+    const json = fs.readFileSync(timeSpacedConfigFilePath)
+
+    return JSON.parse(json)
+}
+
+function createQNA (question, answer) {
+    const qna = {question, answer}
+        
+    data.push(qna)
+
+    saveData(data)
+
+    return qna
+}
+
+const init = function () {
+    //check to see that our file exist
+    // filename is .timespaced in the user's home directory
+    const timeSpacedExists = fs.existsSync(timeSpacedConfigFilePath)
+
+    if (!timeSpacedExists) {
+        console.log('timespacedconfig not found, creating ...')
+        saveData([])
+        console.log('creating timespacedconfig done.')
+    }
+
+    data = loadData()
+
+    console.log('data', data)
+}
+
+const main = function () {
+    console.log(1)
+    init()
+    console.log(2)
+    program.version('0.0.1');
+
+    program
+        .command('add <question> [answer]')
+        .description('clone a repository into a newly created directory')
+        .action((question, answer) => {
+            console.log('clone command called', question, answer);
+            const qna = createQNA(question, answer)
+        });
+
+    program.parse(process.argv);
+}
+
+
+main()
