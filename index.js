3644c1d6d080b15e96e26ed28ffb87e6a4473812
James Lee <uptownhr@gmail.com>
2020-03-20 20:44:24 -0700

    added commanderjs and created add command

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-console.log('hello world from spaced time')
+
+const { program } = require('commander');
+program.version('0.0.1');
+
+program
+    .command('add <question> [answer]')
+    .description('clone a repository into a newly created directory')
+    .action((question, answer) => {
+        console.log('clone command called', question, answer);
+    });
+
+program.parse(process.argv);
