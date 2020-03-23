ef8d69989e60253229280408b1f47ccd40880ddf
James Lee <uptownhr@gmail.com>
2020-03-22 21:26:01 -0700

    added a streak & fibonnaci to askAgainDate calculation

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const os = require('os')
+
+const fibonacci = require('fibonacci')
+const moment = require('moment')
+
 const { program } = require('commander');
+const promptly = require('promptly')
 
 const homeDir = os.homedir()
 const timeSpacedConfigFilePath = homeDir + '/.timespaced'
@@ -29,6 +34,19 @@ function createQNA (question, answer) {
     return qna
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+function getSingleQuestion () {
+    const length = data.length
+
+    return data.find(qna => {
+        if (!qna.askAgainTime) return true
+        return moment(qna.askAgainTime) < moment()
+    })
+}
+
 const init = function () {
     //check to see that our file exist
     // filename is .timespaced in the user's home directory
@@ -41,23 +59,50 @@ const init = function () {
     }
 
     data = loadData()
-
-    console.log('data', data)
 }
 
 const main = function () {
-    console.log(1)
     init()
-    console.log(2)
+
     program.version('0.0.1');
 
     program
         .command('add <question> [answer]')
-        .description('clone a repository into a newly created directory')
+        .description('add a question and answer that you want to remember')
         .action((question, answer) => {
             console.log('clone command called', question, answer);
             const qna = createQNA(question, answer)
         });
+
+    program
+        .command('recall')
+        .description('will display a question for you to recall')
+        .action(async () => {
+            const qna = getSingleQuestion()
+
+            if (!qna) return console.log('no questions to be recalled at this time. Try again later.')
+            
+            console.log(qna.question)
+            
+            await promptly.prompt('Press any key to reveal the answer');
+
+            console.log(qna.answer)
+
+            const remembered = await promptly.choose('did you remember successfully? yes(y) or no(n)', ['y','n'])
+            
+            console.log('remembered', remembered, qna.streak)
+            if (remembered === 'y') { 
+                qna.streak++
+                const {number} = fibonacci.iterate(qna.streak)
+                const askAgainTime = moment().add(number, 'minute')
+                console.log('congrats, we are going to ask you again later', askAgainTime.fromNow())
+                qna.askAgainTime = askAgainTime
+            } else {
+                console.log('sorry we will ask again sooner')
+            }
+
+            saveData(data)
+        })
 
     program.parse(process.argv);
 }
